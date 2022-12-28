fae5b239bd8acb019e56bdc3588ffaf138d4335d
Catalina Simonovich <catalinasimonovich1@gmail.com>
2022-12-28 18:11:51 -0300

    :bookmark: First page version

--- js/main.js
+++ js/main.js
@@ -81,13 +81,17 @@ const listCarrito = (elem) => {
             </div>
             <div class="info-secundaria" id="${elem.id}">
                 <button class=" btn-prod-carrito elimProd bg-danger">
-                        <i class="bi bi-dash"></i>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
+                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+              </svg>
 
                 </button>
             <span id="cant" class="">${elem.cant}</span>
             <button class="btn-prod-carrito agregarUno bg-success">
 
-                     <i class="bi bi-plus-lg fs-6 " ></i>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+          </svg>
 
             </button>
 
--- js/shop.js
+++ js/shop.js
@@ -1,16 +1,19 @@
-const apiShop = 'https://picsum.photos/v2/list'
+const apiShop = 'https://61e71f3dce3a2d0017359624.mockapi.io/articles'
 
 const getUsersAsync = async() => {
     const respose = await fetch(apiShop) 
     const myJson = await respose.json()
     return myJson
 }
-
+const categoryClothes = ['Pantalones', 'Remeras', 'Camperas', 'Buzos', 'Zapatillas', 'Accesorios']
 const renderUsers = async() => {
+    // 
     const shop = await getUsersAsync()
+    //
     const containerCards = document.getElementById('container-cards')
-
+    insertChecks();
     shop.map(clothes => {
+        addCategory(clothes)
         containerCards.insertAdjacentHTML('beforeend', renderCard(clothes))
     })
 }
@@ -18,10 +21,84 @@ renderUsers()
 
 const renderCard = (clothes) => {
     return `
-    <div class="col-6">
+    <div class="col-6" id="${clothes.id}">
         <div class="card h-100">
-            <img src="../assets/2.png" class="card-img-top rounded-1 h-100" alt="${clothes.author}">
+            <img src="../assets/2.png" class="card-img-top rounded-1 h-100" alt="${clothes.name}">
         </div>
+        <label id="${clothes.category}"></label>
     </div>
     `
+}
+
+const elemCheck = (event) => {
+    return `
+            <div class="col-6 col-lg-12">
+                 <div class="form-check mt-2">
+                 <input class="form-check-input" type="checkbox" value="${event}" id="flexCheck" >
+                     <label class="form-check-label" for="flexCheckDefault">
+                         ${event}
+                     </label>
+                 </div>
+             </div>
+             `
+}
+
+// Input Search Filter
+const inputSearchEvents = document.getElementById("input-search-events");
+
+inputSearchEvents.addEventListener("keyup", (event) =>{
+    const allCards = document.querySelectorAll(".card-ctn");
+    //console.log("allCards", allCards)
+    const emptyCardContainer = document.getElementById("emptyContainer");
+    let noResultsCard = ``;
+    //let allCardsArray = Array.prototype.slice.call(allCards);
+    //console.log"allCardsArray", allCardsArray)
+    console.log("event.target.value", event.target.value)
+    // console.log("event.key", event.key)
+    allCards.forEach(card => {
+        console.log(card.id);
+        card.id
+        .toLowerCase()
+        .includes(event.target.value.toLowerCase())
+        ? card.classList.remove("hidden")
+        : card.classList.add("hidden");
+    }); 
+    let eventResult = document.querySelectorAll(".hidden");
+
+    if (eventResult.length === allCards.length){
+        noResultsCard += `
+            <div class="container text-center">
+                <div style="width: 250px; margin: 0 auto">
+                    <img style="width: 100%;" src="#" alt="">
+                </div>
+                <p>Please try another search</p>
+            </div>
+            `
+        }
+    emptyCardContainer.innerHTML = noResultsCard;
+});
+
+const addCategory = (prod) => {
+    //inserto los checks
+
+    //obtengo elemento random de categoryClothes
+    let random = Math.floor(Math.random() * categoryClothes.length);
+
+    //inserto elemento random en prod
+    prod.category = categoryClothes[random];
+
+
+}
+
+
+const insertChecks = () => {
+    //obtengo elemento
+    const containerChecks = document.querySelector('.check-opt');
+    console.log(containerChecks)
+    //inserto elemento
+    categoryClothes.forEach((event) => {
+        containerChecks.insertAdjacentHTML('beforeend', elemCheck(event))
+
+    })
+
 }
