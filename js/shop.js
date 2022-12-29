e753f97f9014dec3dee8d04c2a89d71fea3a6c3c
leandro <leandro.97.ramos@gmail.com>
2022-12-29 15:48:55 -0300

    :sparkles: New Feature: Filter Products

--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,5 @@
-const apiShop = 'https://picsum.photos/v2/list'
+const apiShop = 'https://61e71f3dce3a2d0017359624.mockapi.io/articles'
+const categoryClothes = ['Pantalones', 'Remeras', 'Camperas', 'Buzos', 'Zapatillas', 'Accesorios']
 
 const getUsersAsync = async() => {
     const respose = await fetch(apiShop) 
@@ -7,10 +8,13 @@ const getUsersAsync = async() => {
 }
 
 const renderUsers = async() => {
+    // 
     const shop = await getUsersAsync()
+    //
     const containerCards = document.getElementById('container-cards')
-
+    insertChecks();
     shop.map(clothes => {
+        addCategory(clothes)
         containerCards.insertAdjacentHTML('beforeend', renderCard(clothes))
     })
 }
@@ -18,10 +22,55 @@ renderUsers()
 
 const renderCard = (clothes) => {
     return `
-    <div class="col-6">
+    <div class="col-6" id="${clothes.id}">
         <div class="card h-100">
-            <img src="../assets/2.png" class="card-img-top rounded-1 h-100" alt="${clothes.author}">
+            <img src="../assets/2.png" class="card-img-top rounded-1 h-100" alt="${clothes.name}">
         </div>
+        <label id="${clothes.category}"></label>
     </div>
     `
+}
+
+
+
+const elemCheck = (event) => {
+    return `
+            <div class="col-6 col-lg-12">
+                 <div class="form-check mt-2">
+                 <input class="form-check-input" type="checkbox" value="${event}" id="flexCheck" >
+                     <label class="form-check-label" for="flexCheckDefault">
+                         ${event}
+                     </label>
+                 </div>
+             </div>
+     `
+}
+
+
+const eventFiltroCheck = () => {
+
+    }
+
+const addCategory = (prod) => {
+    //inserto los checks
+    
+    //obtengo elemento random de categoryClothes
+    let random = Math.floor(Math.random() * categoryClothes.length);
+
+    //inserto elemento random en prod
+    prod.category = categoryClothes[random];
+  
+
+}
+
+
+const insertChecks = () => {
+    //obtengo elemento
+    const containerChecks = document.querySelector('.check-opt');
+    //inserto elemento
+    categoryClothes.forEach((event) => {
+        containerChecks.insertAdjacentHTML('beforeend', elemCheck(event))
+        
+    })
+
 }
